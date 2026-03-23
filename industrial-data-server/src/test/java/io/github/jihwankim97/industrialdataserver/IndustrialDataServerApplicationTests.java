be8ee07dd5ea139ea32e1f5de0913f1e9d9b69ac
jihwankim97 <cool102476@naver.com>
2026-03-23 13:55:35 +0900

    feat: industrial-data-server 초기 세팅

---
+++ industrial-data-server/src/test/java/io/github/jihwankim97/industrialdataserver/IndustrialDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jihwankim97.industrialdataserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialDataServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
